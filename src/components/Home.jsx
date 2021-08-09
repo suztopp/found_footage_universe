a2e76ee787a0ffc90e37d7bc8f6613476897e7c6
suztopp <suzannetopp81@gmail.com>
2021-08-09 23:23:42 +1000

    working on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import blairwitch from '../images/foundfootage-blairwitch.jpg';
+import './styling/Home.css';
 
 export default function Home() {
 
 
     return (
         <div>
-            <h1>HOME PAGE</h1>
+            <h1>Welcome to the Dark Side</h1>
+            <img src={blairwitch} className="blairwitch"/>
         </div>
     )
 

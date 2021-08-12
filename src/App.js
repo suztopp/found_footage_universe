fe051852a1dcfbbcf70f46c0600293b6585f8ef4
suztopp <suzannetopp81@gmail.com>
2021-08-12 11:30:30 +1000

    adding in new component pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import AlphabeticalMovies from './components/AlphabeticalMovies';
+import SingleMovie from './components/SingleMovie';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
+
+      <Router>
+        <NavBar />
+        <Switch>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/alphabetical-list" exact component={AlphabeticalMovies} />
+          <Route path="/single-movie" exact component={SingleMovie} />
+          {/* This is to come */}
+          {/* <Route path="/links" exact component={Links} />  */}
+
+          <Route render={() => <h1>404 PAGE NOT FOUND</h1>} />
+
+        </Switch>
+      </Router>
 
     </div>
   );
---
+++ src/components/SingleMovie.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SingleMovie() {
+
+    return (
+        <h1>SINGLE MOVIE PAGE</h1>
+    )
+
+}
